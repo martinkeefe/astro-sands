a2912c44bafc4d5d9b63660f93b1a6d63aca88cf
martinkeefe <martin.keefe@outlook.com>
2024-01-23 01:10:18 +0000

    Enabled web analytics.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({ output: "server", adapter: vercel() });
+export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+});
